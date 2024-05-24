0c45397e4f8eec02b10dbfa16e7a5fd7f2cfb8ed
tangguhlaks <tangguhlaksana0@gmail.com>
2024-05-24 19:50:28 +0700

    inspection test gagal

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -3,19 +3,21 @@ import src.main.java.Counter;
 public class Driver {
 
 	public static void main(String[] args) {
-		
-		Counter counter = new Counter();
-		
-		System.out.println("Current count:" + counter.getCount());
-		
-		counter.increment();
-		
-		System.out.println("Current count:" + counter.getCount());
-		
-		counter.decrement();
-		
-		System.out.println("Current count:" + counter.getCount());
-		
-	}
 
+		Counter counter = new Counter();
+
+		System.out.println("Current count:" + counter.getCount());
+
+		counter.increaseBy(5);
+
+		System.out.println("Current count:" + counter.getCount());
+
+		counter.decreaseBy(2);
+
+		System.out.println("Current count:" + counter.getCount());
+
+		counter.multiplyBy(6);
+
+		System.out.println("Current count:" + counter.getCount());
+	}
 }
